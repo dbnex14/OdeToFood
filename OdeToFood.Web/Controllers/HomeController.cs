4b986fcdfb6ba670b445c364e8caa9c9a24db5f3
Dino Buljubasic <dbuljubasic@squirrelsystems.com>
2019-09-21 12:46:28 -0700

    Added Services with IRestaurantData interface and mock data and showing the list of restaurants in the home page.  Also added logic to show About and Contact page if directed to url http://127.0.0.1:8080/Home/About or http://127.0.0.1:8080/Home/Contact

--- OdeToFood.Web/Controllers/HomeController.cs
+++ OdeToFood.Web/Controllers/HomeController.cs
@@ -4,20 +4,46 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
+using OdeToFood.Data.Services;
 
 namespace OdeToFood.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            var mvcName = typeof(Controller).Assembly.GetName();
-            var isMono = Type.GetType("Mono.Runtime") != null;
+		IRestaurantData db;
 
-            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
-            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+		public HomeController()
+		{
+			db = new InMemoryRestaurantData();
+		}
 
-            return View();
-        }
-    }
+		//     public ActionResult Index()
+		//     {
+		//         var mvcName = typeof(Controller).Assembly.GetName();
+		//         var isMono = Type.GetType("Mono.Runtime") != null;
+
+		//         ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+		//         ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+		//ViewData["Author"] = "Dino Buljubasic";
+
+		//         return View();
+		//     }
+
+		public ActionResult Index()
+		{
+            var model = db.GetAll();
+            return View(model);
+		}
+
+        public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+			return View();
+		}
+
+        public ActionResult Contact()
+		{
+			return View();
+		}
+	}
 }
