4a70807463eb39442a01712b3399493e3d36bec8
Dino Buljubasic <dbuljubasic@squirrelsystems.com>
2019-09-22 21:29:03 -0700

    Added Edit functionality to RestaurantController and added necessary view.

--- OdeToFood.Data/Models/Restaurant.cs
+++ OdeToFood.Data/Models/Restaurant.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace OdeToFood.Data.Models
 {
     public class Restaurant
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Display(Name="Type of food")]
         public CuisineType Cuisine { get; set; }
     }
 }
--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OdeToFood.Data.Models;
 using OdeToFood.Data.Services;
 
 namespace OdeToFood.Web.Controllers
@@ -16,12 +17,14 @@ namespace OdeToFood.Web.Controllers
             this.db = db;
         }
 
+        [HttpGet]
         public ActionResult Index()
         {
             var model = db.GetAll();
-            return View (model);
+            return View(model);
         }
 
+        [HttpGet]
         public ActionResult Details(int id)
         {
             var model = db.Get(id);
@@ -30,6 +33,50 @@ namespace OdeToFood.Web.Controllers
                 return View("NotFound");
             }
             return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Add(restaurant);
+                return RedirectToAction("Details", new { id = restaurant.Id });
+            }
+
+            return View();  // represent with errors to allow user to fix
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var model = db.Get(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Update(restaurant);
+                return RedirectToAction("Details", new { id = restaurant.Id });
+            }
+
+            return View(restaurant);
         }
     }
 }
